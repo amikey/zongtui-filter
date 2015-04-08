96d3f277813305518516c156ae6a75a77ab13a9d
cloudskyme <cloudskyme@163.com>
2015-04-08 15:11:16 +0800

    提交接口

---
+++ filter/src/main/java/com/zongtui/filter/IFilter.java
@@ -0,0 +1,32 @@
+/**
+ * Project Name:filter
+ * File Name:IFilter.java
+ * Package Name:com.zongtui.filter
+ * Date:2015-4-8下午3:04:10
+ * Copyright (c) 2015, 众推项目组版权所有.
+ *
+ */
+package com.zongtui.filter;
+
+/**
+ * ClassName: IFilter <br/>
+ * Function: 过滤器定义接口 <br/>
+ * date: 2015-4-8 下午3:04:10 <br/>
+ *
+ * @author feng
+ * @version 
+ * @since JDK 1.7
+ */
+public interface IFilter {
+	
+	/**
+	 * Similar:判断内容的相似性，为1表示已经存在，为0表示不存在，为浮点数则判断相似度的值. <br/>
+	 *
+	 * @author feng
+	 * @param content
+	 * @return
+	 * @since JDK 1.7
+	 */
+	float Similar(String content);
+
+}
